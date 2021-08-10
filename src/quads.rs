a702c82df29b8ee0835e269308bd47df8c98d22e
Greg Tatum <tatum.creative@gmail.com>
2021-08-09 21:21:52 -0500

    Write docs and fix clippy errors

--- src/quads.rs
+++ src/quads.rs
@@ -1,50 +1,160 @@
+//! This module is a collection of quad geometry creation and manipulation tools. Quads
+//! can be easier to reason with for geometry generation compared to directly manipulating
+//! triangles. In addition, they are useful for generating smoothed models using
+//! Catmull-Clark subdivision.
+
 #![allow(dead_code, unused_imports, unused_variables, unused_mut)]
+#![warn(missing_docs)]
+#![allow(clippy::many_single_char_names)]
+
 use cgmath::prelude::*;
 use cgmath::{vec2, vec3, vec4, Vector2, Vector3, Vector4};
 
+/// An axis direction.
 pub enum Axis {
+    ///
     X,
+    ///
     Y,
+    ///
     Z,
 }
 
+/// The direction of an axis.
 pub enum Direction {
+    ///
     Positive,
+    ///
     Negative,
 }
 
+/// The axis and direction to determine the facing direction of a quad. Useful in the
+/// [SingleQuadOptions].
 pub struct Facing {
-    axis: Axis,
-    direction: Direction,
+    ///
+    pub axis: Axis,
+    ///
+    pub direction: Direction,
 }
 
-type Position = Vector3<f64>;
-type Positions = Vec<Position>;
-type Cell = Vector4<usize>;
-type Cells = Vec<Vector4<usize>>;
+/// A single position in a mesh. It can be indexed by multiple cells.
+pub type Position = Vector3<f64>;
+/// A normal for a mesh.
+pub type Normal = Vector3<f64>;
+/// The collection of [Position]s that are indexed by [Cells].
+pub type Positions = Vec<Position>;
+/// The collection of [Normal]s that match the indexes of the [Positions].
+pub type Normals = Vec<Normal>;
+/// A single cell in a [simplicial complex](https://en.wikipedia.org/wiki/Simplicial_complex).
+pub type Cell = Vector4<usize>;
+/// The list of all cells in the mesh or [simplicial complex](https://en.wikipedia.org/wiki/Simplicial_complex).
+pub type Cells = Vec<Vector4<usize>>;
 
-pub enum CreateQuadsOptions {
-    /// Create a quad from the positions.
+/// The input options for [`create_single_quad`].
+pub enum SingleQuadOptions {
+    /// Create a quad from a positions tuple.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use cgmath::*;
+    /// # use geometry_x::quads::*;
+    /// let mut mesh = QuadMesh::new();
+    /// create_single_quad(
+    ///     &mut mesh,
+    ///     SingleQuadOptions::FromPositions((
+    ///         vec3(-4.0, 0.0, -3.0),
+    ///         vec3(-4.0, 0.0, 3.0),
+    ///         vec3(1.0, 0.0, 3.0),
+    ///         vec3(1.0, 0.0, -3.0),
+    ///     )),
+    /// );
+    ///
+    /// assert_eq!(mesh.as_text_art(Axis::Y).as_str(), &"
+    ///    -5 -4 -3 -2 -1  0  1  2  3  4  5
+    /// -5  ·  ·  ·  ·  ·  ┊  ·  ·  ·  ·  ·
+    /// -4  ·  ·  ·  ·  ·  ┊  ·  ·  ·  ·  ·
+    /// -3  ·  ◆━━━━━━━━━━━━━━◆  ·  ·  ·  ·
+    /// -2  ·  ┃  ·  ·  ·  ┊  ┃  ·  ·  ·  ·
+    /// -1  ·  ┃  ·  ·  ·  ┊  ┃  ·  ·  ·  ·
+    ///  0 ┈┈┈┈┃┈┈┈┈┈┈┈┈┈┈┈┊┈┈┃┈┈┈┈┈┈┈┈┈┈┈┈┈
+    ///  1  ·  ┃  ·  ·  ·  ┊  ┃  ·  ·  ·  ·
+    ///  2  ·  ┃  ·  ·  ·  ┊  ┃  ·  ·  ·  ·
+    ///  3  ·  ◆━━━━━━━━━━━━━━◆  ·  ·  ·  ·
+    ///  4  ·  ·  ·  ·  ·  ┊  ·  ·  ·  ·  ·
+    ///  5  ·  ·  ·  ·  ·  ┊  ·  ·  ·  ·  ·
+    /// "[1..]);
+    /// ```
     FromPositions((Vector3<f64>, Vector3<f64>, Vector3<f64>, Vector3<f64>)),
     /// Create a quad from the width and height of the vector, and the facing direction.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use cgmath::*;
+    /// # use geometry_x::quads::*;
+    /// let mut mesh = QuadMesh::new();
+    /// create_single_quad(
+    ///     &mut mesh,
+    ///     SingleQuadOptions::FromSize((
+    ///         vec2(2.0, 4.0),
+    ///         Facing {
+    ///             axis: Axis::Z,
+    ///             direction: Direction::Positive,
+    ///         },
+    ///     )),
+    /// );
+    ///
+    /// assert_eq!(mesh.as_text_art(Axis::Z).as_str(), &"
+    ///    -5 -4 -3 -2 -1  0  1  2  3  4  5
+    /// -5  ·  ·  ·  ·  ·  ┊  ·  ·  ·  ·  ·
+    /// -4  ·  ·  ·  ·  ·  ┊  ·  ·  ·  ·  ·
+    /// -3  ·  ·  ·  ·  ·  ┊  ·  ·  ·  ·  ·
+    /// -2  ·  ·  ·  ·  ◆━━━━━◆  ·  ·  ·  ·
+    /// -1  ·  ·  ·  ·  ┃  ┊  ┃  ·  ·  ·  ·
+    ///  0 ┈┈┈┈┈┈┈┈┈┈┈┈┈┃┈┈┊┈┈┃┈┈┈┈┈┈┈┈┈┈┈┈┈
+    ///  1  ·  ·  ·  ·  ┃  ┊  ┃  ·  ·  ·  ·
+    ///  2  ·  ·  ·  ·  ◆━━━━━◆  ·  ·  ·  ·
+    ///  3  ·  ·  ·  ·  ·  ┊  ·  ·  ·  ·  ·
+    ///  4  ·  ·  ·  ·  ·  ┊  ·  ·  ·  ·  ·
+    ///  5  ·  ·  ·  ·  ·  ┊  ·  ·  ·  ·  ·
+    /// "[1..]);
+    /// ```
     FromSize((Vector2<f64>, Facing)),
 }
 
+/// A mesh made up of quads.
 #[derive(PartialEq, Debug, Clone)]
 pub struct QuadMesh {
+    /// Indexes into the positions array. A single cell represents a single quad.
     cells: Cells,
+    /// The positions in an unordered Vector.
     positions: Positions,
+    /// The computed normals for a quad mesh.
+    normals: Option<Normals>,
 }
 
 impl QuadMesh {
-    fn new() -> QuadMesh {
+    /// Create a new [QuadMesh] with no normals.
+    pub fn new() -> QuadMesh {
         QuadMesh {
             cells: vec![],
             positions: vec![],
+            normals: None,
         }
     }
 
-    fn get_positions(&self, cell: &Cell) -> (&Position, &Position, &Position, &Position) {
+    /// Create a new [QuadMesh] with normals.
+    pub fn new_with_normals() -> QuadMesh {
+        QuadMesh {
+            cells: vec![],
+            positions: vec![],
+            normals: Some(vec![]),
+        }
+    }
+
+    /// Given a [Cell], look up a [Position]s tuple.
+    pub fn get_positions(&self, cell: &Cell) -> (&Position, &Position, &Position, &Position) {
         (
             self.positions
                 .get(cell.x)
@@ -61,8 +171,40 @@ impl QuadMesh {
         )
     }
 
-    fn iter_cells<'a>(&'a self) -> impl Iterator<Item = &'a Cell> + 'a {
+    /// Iterate through the cells.
+    ///
+    /// ```
+    /// # use geometry_x::quads::*;
+    /// # use cgmath::*;
+    /// let mut mesh = QuadMesh::new();
+    ///
+    /// create_single_quad(
+    ///     &mut mesh,
+    ///     SingleQuadOptions::FromSize((
+    ///         vec2(2.0, 4.0),
+    ///         Facing {
+    ///             axis: Axis::Z,
+    ///             direction: Direction::Positive,
+    ///         },
+    ///     )),
+    /// );
+    ///
+    /// for cell in mesh.iter_cells() {
+    ///    let (a, b, c, d) = mesh.get_positions(&cell);
+    ///    assert_eq!(a, &Vector3::new(1.0, -2.0, 0.0));
+    ///    assert_eq!(b, &Vector3::new(1.0, 2.0, 0.0));
+    ///    assert_eq!(c, &Vector3::new(-1.0, 2.0, 0.0));
+    ///    assert_eq!(d, &Vector3::new(-1.0, -2.0, 0.0));
+    /// }
+    /// ```
+    pub fn iter_cells(&self) -> impl Iterator<Item = &Cell> {
         self.cells.iter()
+    }
+}
+
+impl Default for QuadMesh {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
@@ -76,8 +218,24 @@ fn get_cell_normal(mesh: &QuadMesh, cell: &Cell) -> Vector3<f64> {
     edge_a.cross(edge_b).normalize()
 }
 
-/// Create a quad with options.
-pub fn create_quad(mesh: &mut QuadMesh, options: CreateQuadsOptions) {
+/// See [SingleQuadOptions] for different inputs for creating a single quad inside
+/// of a QuadMesh.
+///
+/// ```text
+///    -5 -4 -3 -2 -1  0  1  2  3  4  5
+/// -5  ·  ·  ·  ·  ·  ┊  ·  ·  ·  ·  ·
+/// -4  ·  ·  ·  ·  ·  ┊  ·  ·  ·  ·  ·
+/// -3  ·  ·  ·  ·  ·  ┊  ·  ·  ·  ·  ·
+/// -2  ·  ·  ·  ·  ◆━━━━━◆  ·  ·  ·  ·
+/// -1  ·  ·  ·  ·  ┃  ┊  ┃  ·  ·  ·  ·
+///  0 ┈┈┈┈┈┈┈┈┈┈┈┈┈┃┈┈┊┈┈┃┈┈┈┈┈┈┈┈┈┈┈┈┈
+///  1  ·  ·  ·  ·  ┃  ┊  ┃  ·  ·  ·  ·
+///  2  ·  ·  ·  ·  ◆━━━━━◆  ·  ·  ·  ·
+///  3  ·  ·  ·  ·  ·  ┊  ·  ·  ·  ·  ·
+///  4  ·  ·  ·  ·  ·  ┊  ·  ·  ·  ·  ·
+///  5  ·  ·  ·  ·  ·  ┊  ·  ·  ·  ·  ·
+/// ```
+pub fn create_single_quad(mesh: &mut QuadMesh, options: SingleQuadOptions) {
     let cell = Vector4::new(
         mesh.positions.len(),
         mesh.positions.len() + 1,
@@ -86,13 +244,13 @@ pub fn create_quad(mesh: &mut QuadMesh, options: CreateQuadsOptions) {
     );
 
     match options {
-        CreateQuadsOptions::FromPositions(positions) => {
+        SingleQuadOptions::FromPositions(positions) => {
             mesh.positions.push(positions.0);
             mesh.positions.push(positions.1);
             mesh.positions.push(positions.2);
             mesh.positions.push(positions.3);
         }
-        CreateQuadsOptions::FromSize((size, facing)) => {
+        SingleQuadOptions::FromSize((size, facing)) => {
             let (x, y) = (size.x / 2.0, size.y / 2.0);
             match facing.axis {
                 Axis::X => {
@@ -119,21 +277,24 @@ pub fn create_quad(mesh: &mut QuadMesh, options: CreateQuadsOptions) {
 
     mesh.cells.push(cell);
     let mut normals = vec![];
-    let normal = get_cell_normal(&mesh, &cell);
+    let normal = get_cell_normal(mesh, &cell);
     normals.push(normal);
     normals.push(normal);
     normals.push(normal);
     normals.push(normal);
 }
 
-trait TextArt {
+/// A trait to draw some piece of geometry using text art. See https://gregtatum.com/writing/2020/ascii-physics-system/
+pub trait TextArt {
+    /// Given a geometry type and an axis, draw it using text and an orthogonal
+    /// projection.
     fn as_text_art(&self, axis: Axis) -> String;
 }
 
 fn get_grid(width: usize, height: usize) -> Vec<String> {
     let mut lines = vec![];
-    let iter = (width as i32 * -1)..(width as i32 + 1);
-    let iter_rev = (height as i32 * -1)..(height as i32 + 1);
+    let iter = -(width as i32)..(width as i32 + 1);
+    let iter_rev = -(height as i32)..(height as i32 + 1);
 
     {
         let mut s = String::from("   ");
@@ -162,12 +323,10 @@ fn get_grid(width: usize, height: usize) -> Vec<String> {
                 } else {
                     s.push_str(" ┊ ");
                 }
+            } else if y == 0 {
+                s.push_str("┈┈┈");
             } else {
-                if y == 0 {
-                    s.push_str("┈┈┈");
-                } else {
-                    s.push_str(" · ");
-                }
+                s.push_str(" · ");
             }
         }
         lines.push(s);
@@ -175,6 +334,7 @@ fn get_grid(width: usize, height: usize) -> Vec<String> {
     lines
 }
 
+/// Safely operate on a UTF-8 string to replace a codepoint.
 fn replace_codepoint(str: &mut String, replacement: &str, index: usize) {
     str.replace_range(
         str.char_indices()
@@ -186,6 +346,7 @@ fn replace_codepoint(str: &mut String, replacement: &str, index: usize) {
 }
 
 impl TextArt for QuadMesh {
+    /// Draw a [QuadMesh] using text art. See https://gregtatum.com/writing/2020/ascii-physics-system/
     fn as_text_art(&self, axis: Axis) -> String {
         let x_margin = 4;
         let y_margin = 1;
@@ -194,7 +355,7 @@ impl TextArt for QuadMesh {
         let half_h: usize = 5;
         let mut lines = get_grid(half_w, half_h);
         for cell in self.iter_cells() {
-            let (a, b, c, d) = self.get_positions(&cell);
+            let (a, b, c, d) = self.get_positions(cell);
 
             let mut translate = |px: f64, py: f64| -> (usize, usize) {
                 (
@@ -245,10 +406,10 @@ impl TextArt for QuadMesh {
                     };
                 };
 
-                print_segment(&a, &b);
-                print_segment(&b, &c);
-                print_segment(&c, &d);
-                print_segment(&d, &a);
+                print_segment(a, b);
+                print_segment(b, c);
+                print_segment(c, d);
+                print_segment(d, a);
             }
 
             {
@@ -264,16 +425,16 @@ impl TextArt for QuadMesh {
                     }
                 };
 
-                print_point(&a);
-                print_point(&b);
-                print_point(&c);
-                print_point(&d);
+                print_point(a);
+                print_point(b);
+                print_point(c);
+                print_point(d);
             }
         }
         let mut string = String::new();
         for item in lines {
             string.push_str(item.trim_end());
-            string.push_str("\n");
+            string.push('\n');
         }
         string
     }
@@ -287,9 +448,9 @@ mod test {
     #[test]
     fn test_create_quad_from_size() {
         let mut mesh = QuadMesh::new();
-        create_quad(
+        create_single_quad(
             &mut mesh,
-            CreateQuadsOptions::FromSize((
+            SingleQuadOptions::FromSize((
                 vec2(2.0, 4.0),
                 Facing {
                     axis: Axis::Z,
@@ -317,9 +478,9 @@ mod test {
     #[test]
     fn test_create_quad_positions() {
         let mut mesh = QuadMesh::new();
-        create_quad(
+        create_single_quad(
             &mut mesh,
-            CreateQuadsOptions::FromPositions((
+            SingleQuadOptions::FromPositions((
                 vec3(-4.0, 0.0, -3.0),
                 vec3(-4.0, 0.0, 3.0),
                 vec3(1.0, 0.0, 3.0),
